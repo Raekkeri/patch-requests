cd87393ccd5360327a69b469dde6f1077ea80b69
Teemu Husso <teemu.husso@gmail.com>
2022-08-11 11:57:49 +0000

    Fix recording
    - patching the 'request' method caused all other methods to be
      recorded twice, fixed this
    - fix reading multiline recording

--- patch_requests.py
+++ patch_requests.py
@@ -64,6 +64,11 @@ class patch_requests(object):
         def record(_actual_http_method, *args, **kwargs):
             getattr(self, f'{_actual_http_method}_requests_patcher').stop()
             getattr(self, f'{_actual_http_method}_session_patcher').stop()
+
+            if _actual_http_method != 'request':
+                getattr(self, 'request_requests_patcher').stop()
+                getattr(self, 'request_session_patcher').stop()
+
             self._counter += 1
 
             import requests
@@ -72,15 +77,17 @@ class patch_requests(object):
             response = getattr(requests, _actual_http_method)(*args, **kwargs)
 
             with open(os.path.join(self.record, (
-                    f'{_actual_http_method}-'
-                    + datetime.datetime.now().strftime('%Y%m%d-%H%M%S%f')
+                    datetime.datetime.now().strftime('%Y%m%d-%H%M%S%f')
                     + f'-{self._counter}'
+                    + f'-{_actual_http_method}'
                     + '.txt')), 'w') as f:
                 f.write(f'{_actual_http_method}\nargs={args}\nkwargs={kwargs}\n')
                 f.write(f'{response.status_code}\n')
                 f.write(response.text)
 
             start_patchers(_actual_http_method)
+            if _actual_http_method != 'request':
+                start_patchers('request')
             return response
 
         def start_patchers(method):
@@ -130,12 +137,12 @@ def load_responses_from_dir(_dir):
     files = sorted(os.listdir(_dir))
     files = (f for f in files if f.endswith('.txt'))
     for file in files:
-        print('reading', file)
         with open(os.path.join(_dir, file)) as f:
-            lines = f.readlines()
+            content = f.read()
+            lines = content.split('\n', 4)
             assert len(lines) == 5
             method = lines[0].strip()
             status_code = int(lines[3])
-            data = lines[4].strip()
+            data = '\n'.join(lines[4:]).strip()
             ret.append((method, (status_code, data)))
     return ret
